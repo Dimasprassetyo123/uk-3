07ffc54987d5af9e85ce34d40dd9c76053d0dc3f
Dimasprassetyo123 <dimasprassetyo108@gmail.com>
2025-10-24 19:34:07 +0700

    motong template fronendt

---
+++ frontend/partials/script.php
@@ -0,0 +1,12 @@
+  <!-- Vendor JS Files -->
+  <script src="./template_user/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="./template_user/assets/vendor/php-email-form/validate.js"></script>
+  <script src="./template_user/assets/vendor/aos/aos.js"></script>
+  <script src="./template_user/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="./template_user/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="./template_user/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="./template_user/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="./template_user/assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="./template_user/assets/js/main.js"></script>
